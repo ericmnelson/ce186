5bf0c2d604e529289e278f6db3f1a13ffebde977
ericmnelson <ericn@berkeley.edu>
2016-11-21 16:46:58 -0800

    work on showers dashboard

--- showerlyze/config/routes.rb
+++ showerlyze/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :houses
   resources :users
+  resources :bathrooms
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
